9215f243dbca4e6afb0aa7c445195c621ce8d17a
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-23 13:01:08 +0100

    add exercise complete

---
+++ snack-3/assets/js/main.js
@@ -0,0 +1,13 @@
+//Crea un array di oggetti che rappresentano degli animali.
+//Ogni animale ha un nome, una famiglia e una classe.
+
+const arrayAnimal = [
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+//Crea un nuovo array con la lista dei mammiferi.
+
+const arrayMammiferi = arrayAnimal.filter(animal => animal.classe === 'mammiferi');
+
+console.log(arrayMammiferi);
